e5bed20ee7a3cd6ccf648d4cfd6426b4c506fb64
mlfcjob@163.com <mlfcjob@163.com>
2017-10-18 16:35:20 +0800

    Add ffmpeg's dependency to the project.
    Add ffmpeg's dependency to the project, and use the first Qt
    project to apply.
    
    Signed-off-by: mlfcjob@163.com <mlfcjob@163.com>

--- windows/code/src/firstQt/main.cpp
+++ windows/code/src/firstQt/main.cpp
@@ -1,8 +1,17 @@
 #include "firstQt.h"
 #include <QtWidgets/QApplication>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avcodec.lib")
+
 int main(int argc, char *argv[])
 {
+	av_register_all();
 	QApplication a(argc, argv);
 	firstQt w;
 	w.show();
